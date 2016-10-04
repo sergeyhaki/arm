13c0daa98c620ea0191e46033e7349b7789e95a7
sergeyhaki <navent@mail.ru>
2016-10-04 20:01:38 +0300

    2016 10 04

--- old/git add.bat
+++
@@ -1,7 +0,0 @@
-call git.exe add . 
-
-call git.exe status
-
-call git.exe commit -m "from bat"
-
-pause
--- old/git init.bat
+++
@@ -1,5 +0,0 @@
-call git.exe init
-
-git remote add origin https://github.com/sergeyhaki/arm.git
-
-pause
